b7cfe1b8fe5461ef1876a8f0e2532af982861d9f
alfreding0 <alfredogonzales953@gmail.com>
2020-11-24 18:03:16 -0400

    hecho la capa de datos de estudiante

--- src/datos/Estudiante.java
+++ src/datos/Estudiante.java
@@ -1,14 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package datos;
-
-/**
- *
- * @author alfred
- */
 public class Estudiante {
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String ci;
+    private String sexo;
+    private String telefono;
+
+    public Estudiante() {
+        this.id = "";
+        this.nombre = "";
+        this.apellido = "";
+        this.ci = "";
+        this.sexo = "";
+        this.telefono = "";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
     
+    
+    
+    public String insertar()
+    {
+        return "INSERT INTO estudiante (nombre, apellido, ci, sexo, telefono) VALUES ('"+nombre+"','"+apellido+"','"+ci+"','"+sexo+"','"+telefono+"');";
+    }
+    public String actualizar()
+    {
+        return "UPDATE estudiante SET nombre='"+nombre+"', apellido='"+apellido+"', ci='"+ci+"', sexo='"+sexo+"', telefono='"+telefono+"' WHERE id="+id+";";
+    }        
+    public String eliminar()
+    {
+        return "DELETE from estudiante WHERE id="+id+";";
+    }        
+    public String buscar()
+    {
+        return "SELECT * FROM estudiante;";
+    }
 }
