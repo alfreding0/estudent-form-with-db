5485440e493de79ae7c639e657c5549c3c1aff77
alfreding0 <alfredogonzales953@gmail.com>
2020-11-24 18:42:04 -0400

    hecho el main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,23 @@
 package main;
+
+import presentacion.FormEstudiante;
+
 /** @author Curso de Git*/
 public class Main {
     public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FormEstudiante.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
         
+        FormEstudiante estudent = new FormEstudiante();
+        estudent.show();
     }
     
 }
